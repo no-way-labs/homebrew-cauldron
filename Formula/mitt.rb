4ebefcf5a9865e4891a624f6f2deb5bbb97a4c62
Keston Aquino-Michaels <kestonamichaels@gmail.com>
2026-07-13 21:52:54 -0400

    Update mitt formula for v0.5.0

--- Formula/mitt.rb
+++ Formula/mitt.rb
@@ -1,26 +1,26 @@
 class Mitt < Formula
   desc "Encrypted file transfer CLI tool"
   homepage "https://github.com/no-way-labs/cauldron"
-  version "0.4.2"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/no-way-labs/cauldron/releases/download/v0.4.2/mitt-macos-aarch64.tar.gz"
-      sha256 "b4e84b8eb19aeb192039d975a6d0ed1fdd5d2f182c39ae5e3bc7f29299dc073e"
+      url "https://github.com/no-way-labs/cauldron/releases/download/v0.5.0/mitt-macos-aarch64.tar.gz"
+      sha256 "f98d5333a3d8acb6e4b7bc82b4b99792d17e471f6f26831975d9bc2f91d2db01"
     else
-      url "https://github.com/no-way-labs/cauldron/releases/download/v0.4.2/mitt-macos-x86_64.tar.gz"
-      sha256 "b2e719833e2f1d7b457af5728dc2ce72a37eff82f1fcecc1d237f8db0586812d"
+      url "https://github.com/no-way-labs/cauldron/releases/download/v0.5.0/mitt-macos-x86_64.tar.gz"
+      sha256 "093074978f66df0b9e4c71818aab6b70413f723b19a5fef60e2288a64ae07c84"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/no-way-labs/cauldron/releases/download/v0.4.2/mitt-linux-aarch64.tar.gz"
-      sha256 "8333853c70bcbfff00ffdacf7852540fde2b88993c9e28c1c2a1c4ba58352d1e"
+      url "https://github.com/no-way-labs/cauldron/releases/download/v0.5.0/mitt-linux-aarch64.tar.gz"
+      sha256 "9c0b313efd1a8dead69917b35b77669fa1556254428ac4644c85c3cffa07f378"
     else
-      url "https://github.com/no-way-labs/cauldron/releases/download/v0.4.2/mitt-linux-x86_64.tar.gz"
-      sha256 "1e3abfee07409d6f19db82d66e9f9cc86b2abc754a1f0402d82fc21f68158037"
+      url "https://github.com/no-way-labs/cauldron/releases/download/v0.5.0/mitt-linux-x86_64.tar.gz"
+      sha256 "d7fcdb2272fa0130187da6064839313e56e318d85a9d493345df7f8d1a545b2b"
     end
   end
 
